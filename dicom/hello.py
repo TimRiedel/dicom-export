467c2e62c9e50f94acb4e466b98efd0c3c44eea2
Tim Riedel <timriedel00@gmail.com>
2022-11-25 09:50:37 +0100

    load arbitrary python files

--- dicom/hello.py
+++ dicom/hello.py
@@ -1,2 +1,2 @@
 def helloWorld():
-  print("Hello World")
+  print("Hello World from Python!")
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
-import { loadPyodide } from "pyodide";
+import { loadPyodide, PyodideInterface } from "pyodide";
 const fetch = require("node-fetch");
 
-async function load() {
+async function loadPythonFile(filePath: string) {
+    const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+    const moduleName = fileName.substring(0, fileName.lastIndexOf("."));
+
+    //TODO: Pyodide is loaded multiple times. Is this necessary?
     let pyodide = await loadPyodide();
-    let response = await fetch("http://localhost:8000/dicom/benchmark.py");
+    let response = await fetch("http://localhost:8000/" + filePath);
     let buffer = await response.text();
-    pyodide.FS.writeFile("benchmark.py", buffer);
-    // const readFile = pyodide.FS.readFile("benchmark.py", {
+    pyodide.FS.writeFile(fileName, buffer);
+    // const readFile = pyodide.FS.readFile(fileName, {
     //     encoding: "utf8",
     // });
-    return pyodide.pyimport("benchmark");
+    return pyodide.pyimport(moduleName);
 }
 
 async function main() {
-    const benchmark = await load();
-    console.log("Import successful");
-    benchmark.execute();
+    const hello = await loadPythonFile("dicom/hello.py");
+    const benchmark = await loadPythonFile("dicom/benchmark.py");
+    hello.helloWorld();
+    benchmark.execute(100000);
 }
 
 main();
